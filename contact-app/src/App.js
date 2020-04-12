fdd49fb8581768626d1c687505200a6add8e1f9a
kimji9663 <rlawlgp9663@naver.com>
2020-04-12 17:57:26 +0900

    Add contact-app directory

---
+++ contact-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <PhoneForm />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ contact-app/src/components/PhoneForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+
+    state = {
+        name: '', // = value={this.state.name}
+        phone: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            // onChange가 실행되는 e.target은 input이므로, 그 하위 값 value를 넣어 줌 
+            // name: e.target.value,
+            // phone: e.target.value,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    render() {
+        return (
+            //단일 태그로 감싸주어야 함. form이던 div던 상관없이.
+            <form>
+                {/* input이 여러개 일 때에는 name값을 지정해서 handleChange 값을 변경한다. */}
+                <input 
+                    placeholder="이름" 
+                    name="name" 
+                    onChange={this.handleChange} 
+                    value={this.state.na}
+                />
+                <br />
+                <input 
+                    placeholder="전화번호" 
+                    name="phone" 
+                    onChange={this.handleChange}
+                />
+                <br />
+                {this.state.name}
+                <br />
+                {this.state.phone}
+            </form>
+        );
+    }
+}
+
+export default PhoneForm;
